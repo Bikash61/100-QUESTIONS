40fa090ca03c13c50d9b12dceca62c8bc4833fb6
Bikash61 <bashyalb96@gmail.com>
2024-07-15 23:24:58 +0545

    Updating the work

---
+++ PYTHON 100 QUESTIONS/fibonacchiSeries.py
@@ -0,0 +1,7 @@
+def fibonachi(n):
+    fib_seq =[0,1]
+    while len(fib_seq)<n:
+        fib_seq.append(fib_seq[-1]+fib_seq[-2])
+    return fib_seq[:n]
+
+print("The fibonachi series is",fibonachi(15))
